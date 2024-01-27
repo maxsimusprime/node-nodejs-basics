a419a2714a8f242f630551e61719410805cf8af2
maxsimusprime <96006023+maxsimusprime@users.noreply.github.com>
2024-01-27 16:58:54 +0300

    feat: add decision to cli assignment

--- src/cli/args.js
+++ src/cli/args.js
@@ -1,5 +1,18 @@
 const parseArgs = () => {
-    // Write your code here 
+
+  const { argv } = process;
+
+  const res = argv
+    .slice(2)
+    .reduce((acc, el, id, arr) => {
+        if (el.startsWith('--') && !arr[el+1]?.startsWith('--')) {
+            return [...acc, `${el.slice(2)} is ${arr[id+1]}`];
+        }
+        return acc;
+    }, [])
+    .join(', ');
+
+  console.log(res);
 };
 
 parseArgs();
--- src/cli/env.js
+++ src/cli/env.js
@@ -1,5 +1,12 @@
 const parseEnv = () => {
-    // Write your code here 
+  const { env } = process;
+
+  const res = Object.keys(env)
+    .filter((key) => key.startsWith("RSS_"))
+    .map((key) => `${key}=${env[key]}`)
+    .join('; ');
+
+  console.log(res);
 };
 
 parseEnv();
