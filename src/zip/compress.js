4e4c06b78362330ece590cb64b43f5e366ea8a1b
maxsimusprime <96006023+maxsimusprime@users.noreply.github.com>
2024-01-28 17:32:46 +0300

    feat: add decision to zlib assignment

--- src/zip/compress.js
+++ src/zip/compress.js
@@ -1,5 +1,16 @@
+import { createReadStream, createWriteStream } from "node:fs";
+import { createGzip } from 'zlib';
+
 const compress = async () => {
-    // Write your code here 
+  const fileToCompress = "./src/zip/files/fileToCompress.txt";
+  const archive = "./src/zip/files/archive.gz";
+
+  const readStream = createReadStream(fileToCompress);
+  const writeStream = createWriteStream(archive);
+
+  readStream
+    .pipe(createGzip())
+    .pipe(writeStream);
 };
 
 await compress();
--- src/zip/decompress.js
+++ src/zip/decompress.js
@@ -1,5 +1,16 @@
+import { createReadStream, createWriteStream } from "node:fs";
+import { createGunzip } from "node:zlib";
+
 const decompress = async () => {
-    // Write your code here 
+  const archive = "./src/zip/files/archive.gz";
+  const fileToCompress = "./src/zip/files/fileToCompress.txt";
+
+  const readStream = createReadStream(archive);
+  const writeStream = createWriteStream(fileToCompress);
+
+  readStream
+    .pipe(createGunzip())
+    .pipe(writeStream);
 };
 
 await decompress();
