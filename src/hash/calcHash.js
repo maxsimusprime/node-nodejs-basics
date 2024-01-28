44ca6fa74bc27fb62f2d9b5cb89b2653e49d0114
maxsimusprime <96006023+maxsimusprime@users.noreply.github.com>
2024-01-28 17:14:00 +0300

    feat: add decision to hash assignment

--- src/hash/calcHash.js
+++ src/hash/calcHash.js
@@ -1,5 +1,16 @@
+import { createReadStream } from "node:fs";
+import { createHash } from "node:crypto";
+
 const calculateHash = async () => {
-    // Write your code here 
+  const { stdout } = process;
+
+  const file = "./src/hash/files/fileToCalculateHashFor.txt";
+
+  const stream = createReadStream(file, "utf-8");
+
+  stream
+    .pipe(createHash("sha256").setEncoding("hex"))
+    .pipe(stdout);
 };
 
 await calculateHash();
