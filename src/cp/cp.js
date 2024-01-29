0d6f4f512b2a54b2df00b67a01ebcde38a6d9a42
maxsimusprime <96006023+maxsimusprime@users.noreply.github.com>
2024-01-29 20:37:45 +0300

    feat: add decision to cp assignment

--- src/cp/cp.js
+++ src/cp/cp.js
@@ -1,6 +1,12 @@
+import { spawn } from "node:child_process";
+
 const spawnChildProcess = async (args) => {
-    // Write your code here
+  const command = "node";
+  const pathArg = "./src/cp/files/script.js";
+
+  spawn(command, [pathArg, ...args], {
+    stdio: ["inherit", "inherit", "inherit", "ipc"],
+  });
 };
 
-// Put your arguments in function call to test this functionality
-spawnChildProcess( /* [someArgument1, someArgument2, ...] */);
+spawnChildProcess(["1", "2", "3"]);
