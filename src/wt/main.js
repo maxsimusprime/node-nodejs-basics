e601edb8164cb45db2d96127872144ff111c847c
maxsimusprime <96006023+maxsimusprime@users.noreply.github.com>
2024-01-29 15:48:05 +0300

    feat: add decision to wt assignment

--- src/wt/main.js
+++ src/wt/main.js
@@ -1,5 +1,22 @@
+import { cpus } from "node:os";
+import { Worker } from "node:worker_threads";
+
 const performCalculations = async () => {
-    // Write your code here
+  const numberOfCpuCore = cpus().length; 
+  // or os.availableParallelism()
+
+  const poolOfWorkers = [...new Array(numberOfCpuCore)]
+    .map((_, index) => index + 10)
+    .map(
+      (workerData) =>
+        new Promise((resolve) => {
+          const worker = new Worker("./src/wt/worker.js", { workerData });
+          worker.on("message", (data) => resolve({ status: "resolved", data }));
+          worker.on("error", () => resolve({ status: "error", data: null }));
+        })
+    );
+
+  Promise.all(poolOfWorkers).then((result) => console.log(result));
 };
 
 await performCalculations();
